d48bc83ce706e0e511860e6bee6c6946bb49378b
Chris Nappi <xnappo@jupiter.localdomain>
2023-10-29 10:31:30 -0500

    Added ability to add HDR10+ to standard EZCOO EDID, added script to extract EDIDs from FW

---
+++ edid_fw_parse.py
@@ -0,0 +1,43 @@
+import re
+import sys
+
+def check_dolby_vision(ecid_data):
+    if 'EB0146D000' in ecid_data:
+        print('Dolby Vision Capable')
+        print_ecid(ecid)
+        print('\n' * 2)  # Two line breaks between EDIDs
+        
+# Function to format and print the ECID data
+def print_ecid(ecid_data):
+    for i in range(0, len(ecid_data), 32):
+        print(ecid_data[i:i+32])
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python script.py <hex_file>")
+    sys.exit(1)
+
+# Get the filename from the command-line arguments
+hex_file = sys.argv[1]
+
+# Read the Intel Hex file
+with open(hex_file, 'r') as file:
+    lines = file.readlines()
+
+ecid_data = ''  # Variable to store ECID data
+start_lines = []
+
+# Iterate through the lines of the hex file
+for line in lines:
+    data = line[9:-3]  # Extract data portion, excluding address and checksum
+    ecid_data += data
+
+# Search for all occurrences of '00ffffffffffff' pattern in the concatenated data (case-insensitive)
+matches = re.finditer('00ffffffffffff', ecid_data, re.IGNORECASE)
+
+for match in matches:
+    start_index = match.start()
+    end_index = start_index + 512
+
+    ecid = ecid_data[start_index:end_index]
+    check_dolby_vision(ecid)
